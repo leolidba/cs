ed6ae216d8bf2c92bc61ed2c365e2b552a7436e0
leoli2 <leoli2@ebay.com>
2018-07-23 00:46:15 -0700

    TDD starts

--- modules/com.codersmart.algorithms/trainings/src/test/java/com/codersmart/algorithms/trainings/TwoSumTest.java
+++ modules/com.codersmart.algorithms/trainings/src/test/java/com/codersmart/algorithms/trainings/TwoSumTest.java
@@ -1,8 +1,10 @@
 package com.codersmart.algorithms.trainings;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
 
 public class TwoSumTest {
 
@@ -16,5 +18,7 @@ public class TwoSumTest {
 
     @Test
     public void getTwoSum() {
+        Assertions.assertArrayEquals(new int[]{0}, new int[] {1});
     }
+
 }
