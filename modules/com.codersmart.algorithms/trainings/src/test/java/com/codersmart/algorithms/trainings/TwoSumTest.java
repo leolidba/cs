5a95e702916d9141b9841190e6b63c7cd40c8094
leoli2 <leoli2@ebay.com>
2018-07-23 00:15:21 -0700

    Ready to add tests

---
+++ modules/com.codersmart.algorithms/trainings/src/test/java/com/codersmart/algorithms/trainings/TwoSumTest.java
@@ -0,0 +1,20 @@
+package com.codersmart.algorithms.trainings;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void getTwoSum() {
+    }
+}
