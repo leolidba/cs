e4babbd47fc0c85e59bba118ec0fa8f815b2b2f0
leoli2 <leoli2@ebay.com>
2018-07-22 23:45:29 -0700

    Trival IDE fies

---
+++ modules/com.codersmart.algorithms/trainings/src/main/java/com/codersmart/algorithms/trainings/TwoSum.java
@@ -0,0 +1,9 @@
+package com.codersmart.algorithms.trainings;
+
+public class TwoSum {
+    public static int[] getTwoSum(int[] input, int target){
+        int[] ret = new int[]{};
+        return ret;
+    }
+}
+
