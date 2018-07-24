bef895de2ab03cdae66b09236285692625d252cf
leoli2 <leoli2@ebay.com>
2018-07-23 23:08:55 -0700

    Save point

--- modules/com.codersmart.algorithms/trainings/src/test/java/com/codersmart/algorithms/trainings/TwoSumTest.java
+++ modules/com.codersmart.algorithms/trainings/src/test/java/com/codersmart/algorithms/trainings/TwoSumTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import sun.jvm.hotspot.utilities.Assert;
 
 public class TwoSumTest {
 
@@ -18,7 +17,11 @@ public class TwoSumTest {
 
     @Test
     public void getTwoSum() {
-        Assertions.assertArrayEquals(new int[]{2, 5}, new TwoSum().getTwoSum(new int[] {1, 2, 3, 4, 5, 7, 11}, 10));
+        Assertions.assertArrayEquals(new int[]{2, 5}, TwoSum.getTwoSum(new int[] {1, 2, 3, 4, 5, 7, 11}, 10));
     }
 
+    @Test
+    void getTwoSumBest() {
+        Assertions.assertArrayEquals(new int[]{2, 5}, new TwoSum().getTwoSumBest(new int[] {1, 2, 3, 4, 5, 7, 11}, 10));
+    }
 }
